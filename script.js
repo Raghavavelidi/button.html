5977891e3dc3e489784dc45d8318de7254bcc798
Raghavavelidi <135940607+Raghavavelidi@users.noreply.github.com>
2023-07-19 15:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://freepngimg.com/save/31669-bulb-off-transparent-image/500x500"
+
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-320-80.jpg"
+
+}
